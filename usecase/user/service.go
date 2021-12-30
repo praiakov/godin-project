cc040b0441f38cda4fc8e0059ba0e2c24b8d5416
praiakov <adrianopraia@live.com>
2021-12-29 22:41:16 -0300

    changes

--- usecase/user/service.go
+++ usecase/user/service.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"errors"
 	"time"
 
 	"github.com/praiakov/godin/entity"
@@ -25,4 +26,24 @@ func (s *Service) CreateUser(name, email string, totalMonth int) (entity.ID, err
 	e := entity.NewUser(name, email, data, data.AddDate(0, totalMonth, 0), totalMonth)
 
 	return s.repo.Create(e)
+}
+
+//CreateUser Create an user
+func (s *Service) GetUser(id entity.ID) (*entity.User, error) {
+	return s.repo.Get(id)
+}
+
+//DeleteUser Delete an user
+func (s *Service) DeleteUser(id entity.ID) error {
+	u, err := s.GetUser(id)
+
+	if u == nil {
+		return errors.New("Not found")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return s.repo.Delete(id)
 }
